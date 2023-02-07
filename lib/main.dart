0c80bac82e1b028137b58f3e792c85a207310d9c
Nhut Linh <trannhutlinh0925@gmail.com>
2023-02-08 00:33:49 +0700

    Xay dung trang chi tiet san pham

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'ui/products/products_manager.dart';
+import 'ui/products/product_detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,12 +22,17 @@ class MyApp extends StatelessWidget {
           secondary: Colors.deepOrange,
         ),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('MyShop'),
-        ),
-        body: const Center(
-          child: Text('Welcome to MyShop'),
+      // home: Scaffold(
+      //   appBar: AppBar(
+      //     title: const Text('MyShop'),
+      //   ),
+      //   body: const Center(
+      //     child: Text('Welcome to MyShop'),
+      //   ),
+      // ),
+      home: SafeArea(
+        child: ProductDetailScreen(
+          ProductsManager().items[0],
         ),
       ),
     );
